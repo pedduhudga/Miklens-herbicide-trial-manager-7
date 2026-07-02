779114c4db26db9312519766d17f37084c9d65e5
pedduhudga <pedduhudga@github.com>
2026-07-03 00:11:12 +0530

    fix: change jsPDF import to default import in compareReports.js to fix TypeError constructor in Vite production bundle

--- src/services/compareReports.js
+++ src/services/compareReports.js
@@ -1,4 +1,4 @@
-import { jsPDF } from 'jspdf';
+import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
 import { safeJsonParse } from '../utils/helpers.js';
 import { formatDateTime } from '../utils/dateUtils.js';
