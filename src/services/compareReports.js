3dac09f1c909d8c0a668f7df7c8da8e641bf9cd9
pedduhudga <pedduhudga@github.com>
2026-07-03 00:03:50 +0530

    fix: import jspdf-autotable in compareReports.js to fix TypeError autoTable is not a function

--- src/services/compareReports.js
+++ src/services/compareReports.js
@@ -1,4 +1,5 @@
 import { jsPDF } from 'jspdf';
+import autoTable from 'jspdf-autotable';
 import { safeJsonParse } from '../utils/helpers.js';
 import { formatDateTime } from '../utils/dateUtils.js';
 import { getCategoryConfig, getPrimaryObservationField } from '../utils/categoryConfig.js';
